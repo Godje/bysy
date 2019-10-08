8e4e41cf8adb6bd5cd263f6aec691dce3c536db2
Godje <comepot.4anell@gmail.com>
2019-10-07 20:52:46 -0700

    Aliases displayed in help

--- core/printhelp.py
+++ core/printhelp.py
@@ -9,12 +9,12 @@ def printhelp(args):
 
 Available methods:{1}
 
-	{0}start{1}		- (Sector, Project, Description) starts a new Log
-	{0}stop{1}		- stops the last log
-	{0}list{1}		- lists logs
-	{0}time{1}		- tells how much time has passed since the last log
-	{0}delete{1}		- delets a selected log
-	{0}resume{1}		- resumes a the last task. Or the task at <id> you specify
-	{0}help{1}		- displays this help message
+	{0}start{1}\t\t\t- (Sector, Project, Description) starts a new Log
+	{0}stop|pause{1}\t\t- stops the last log
+	{0}list|ls|log{1}\t\t- lists logs
+	{0}time{1}\t\t\t- tells how much time has passed since the last log
+	{0}delete{1}\t\t\t- delets a selected log
+	{0}resume{1}\t\t\t- resumes a the last task. Or the task at <id> you specify
+	{0}help{1}\t\t\t- displays this help message
 		"""
 	print help_string.format(txtmodif.BOLD, txtmodif.NORMAL)
