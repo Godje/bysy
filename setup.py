2c8282e0982e7b197e73c5783483cd5772102adf
Daniel Mayovskiy <comepot.4anell@gmail.com>
2019-10-25 10:55:51 -0700

    setup update. gitignore update

--- setup.py
+++ setup.py
@@ -8,9 +8,16 @@ setup(
 		url="https://github.com/Godje/bysy",
 		author="Daniel Mayovskiy",
 		author_email="daniel.mayovskiy@gmail.com",
-		license="MIT",
+		license="MPL 2.0",
 		packages=["bysy"],
+		keywords=["time", "logging", "management"],
 		zip_safe=False,
 		entry_points={"console_scripts": ["bysy=bysy.__main__:main"]},
-		include_package_data=True
+		include_package_data=True,
+		classifiers=[
+			"Development Status :: 4 - Beta",
+			"Intended Audience :: Developers",
+			# "License :: Mozilla Public License 2.0",
+			# "Programming Language :: Python :: 2.7.15"
+			]
 		);
