3e96147a4a2f709fadce5d04bce40eadfa987647
Daniel Mayovskiy <comepot.4anell@gmail.com>
2019-11-01 12:02:23 -0700

    updating the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import bysy;
 
 setup(
 		name="bysy",
-		version="0.8.2",
+		version="0.8.3",
 		description="Self-management time logging tool",
 		url="https://github.com/Godje/bysy",
 		author="Daniel Mayovskiy",
