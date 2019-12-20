850eff7b3577e70f698d2710fb3dd27c444d3f71
Daniel Mayovskiy <comepot.4anell@gmail.com>
2019-12-20 14:49:17 -0800

    Pip RST with a link to Wiki

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
-from setuptools import setup;
 import bysy;
+
+import codecs;
+import os;
+import re;
+import sys;
+
+from setuptools import find_packages, setup;
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+long_description = read('README.rst');
 
 setup(
 		name="bysy",
-		version="0.9.2",
+		version="1.0",
 		description="Self-management time logging tool",
+		long_description=long_description,
 		url="https://github.com/Godje/bysy",
 		author="Daniel Mayovskiy",
 		author_email="daniel.mayovskiy@gmail.com",
@@ -16,8 +33,10 @@ setup(
 		include_package_data=True,
 		classifiers=[
 			"Development Status :: 4 - Beta",
+			"Environment :: Console",
 			"Intended Audience :: Developers",
-			# "License :: Mozilla Public License 2.0",
-			# "Programming Language :: Python :: 2.7.15"
+			"License :: Mozilla Public License 2.0",
+			"Programming Language :: Python :: 2.7.15",
+			"Topic :: Time Tracking"
 			]
 		);
